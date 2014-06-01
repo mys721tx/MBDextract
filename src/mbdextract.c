0410265c57a6a3ef2e777dd56cf41048b32cdc6a
Yishen Miao <mys721tx@gmail.com>
2014-06-01 15:53:28 -0700

    Init comment
    MBDextract 0.1 is a tool for extracting game scripts from MBD files.
    
    new file:   .gitignore
    new file:   .travis.yml
    new file:   COPYING
    new file:   Makefile.am
    new file:   Makefile.in
    new file:   README.md
    new file:   aclocal.m4
    new file:   compile
    new file:   config.h.in
    new file:   configure
    new file:   configure.ac
    new file:   depcomp
    new file:   install-sh
    new file:   missing
    new file:   src/Makefile.am
    new file:   src/Makefile.in
    new file:   src/mbdextract.c

---
+++ src/mbdextract.c
@@ -0,0 +1,244 @@
+#include <config.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+const char *ver_str = PACKAGE_STRING;
+const char *bugreport = PACKAGE_BUGREPORT;
+
+void printUsage(char pname[])
+{
+	printf("%s\nUsage: %s file\n\nplease send bug reports to <%s>.\n", 
+			ver_str, pname, bugreport);
+}
+
+
+int main(int argc, char *argv[])
+{
+	FILE *ifp;
+	char *mode = "rb";
+	char *fbuff;
+	unsigned long ifp_len;
+	int i;
+	unsigned long offset;
+
+	if (argc < 2) {
+		printUsage(argv[0]);
+		return 0;
+	}
+
+	ifp = fopen(argv[1], mode);
+	if (!ifp) {
+		fprintf(stderr, "Unable to open %s: %s\n", 
+				argv[1], strerror(errno));
+		return errno;
+	}
+
+	fseek(ifp, 0, SEEK_END);
+	ifp_len = ftell(ifp);
+	rewind(ifp);
+
+	fbuff = (char *)malloc(ifp_len+1);
+	if (!fbuff) {
+		fprintf(stderr, "Unable to allocate memory: %s\n", 
+				strerror(errno));
+		fclose(ifp);
+		return errno;
+	}
+
+	fread(fbuff, ifp_len, 1, ifp);
+	fclose(ifp);
+
+	offset = 0;
+	while (1) {
+		if (fbuff[offset] == 'M') {
+			if (fbuff[offset + 1] == 'B') {
+				if (fbuff[offset + 2] == 'D') {
+					if(fbuff[offset + 3] == '\0') {
+						break;
+					}
+				}
+			}
+		}
+		offset++;
+	}
+	
+    printf("<MDB>");
+/*
+ * The converation talbe comes from 
+ * http://forums.qhimm.com/index.php?topic=8163.msg98625#msg98625
+ */
+
+	for (i = offset + 4; i < ifp_len; i+= 4){
+        	switch (fbuff[i]) {
+            	    case (char)0x01:
+                	printf("<STRING>\n");
+                	break;
+            	    case (char)0x0c:
+                	if (fbuff[i - 4] <= (char)0xff) {
+                    	    printf("<%dTEXT>", fbuff[i - 4] & ~ (~0 << 2));
+                	}
+                	printf("<???TEXT>");
+                	break;
+            	    case (char)0x40:
+                	switch (fbuff[i + 1]) {
+                    	    case (char)0x02:
+                        printf("<NL>\n");
+                        break;
+                    case (char)0x03:
+                        printf("<NUK1>");
+                        break;
+                    case (char)0x04:
+                        printf("<END>\n\n");
+                        break;
+                    case (char)0x05:
+                        if (fbuff[i + 3] == (char)0x01) {
+                            printf("<CHOICE2>");
+                        } else {
+                            printf("<M-CHOICE%d>", fbuff[i + 3]);
+                        }
+                        break;
+                    case (char)0x06:
+                        printf("<CROSS-PAD>");
+                        break;
+                    case (char)0x07:
+                        printf("<CROSS-SQUARE>");
+                        break;
+                    case (char)0x10:
+                        printf("<CHOICE1>");
+                        break;
+                    case (char)0x11:
+                        printf("<VAR4>");
+                        break;
+                    case (char)0x12:
+                        printf("<VAR5>");
+                        break;
+                    case (char)0x2b:
+                        printf("<VAR1>");
+                        break;
+                    case (char)0x2c:
+                        printf("<VAR2>");
+                        break;
+                    case (char)0x2d:
+                        printf("<VAR3>");
+                        break;
+                    case (char)0x36:
+                        printf("<FUNCTION1>");
+                        break;
+                }
+                break;
+            case (char)0x80:
+/*
+ * 0x80000000 - 0x80001900 are capital case letters.
+ * 0x80002000 - 0x80003300 are lower case letters.
+ * 0x80003400 - 0x80003D00 are digits.
+ * 0x80003E00 - 0x80005A00 are symbols.
+ */
+                if (fbuff[i + 2] < (char)0x1a) {
+                    printf("%c", (char)((int)fbuff[i + 2] + 0x41));
+                } else if (fbuff[i + 2] < (char)0x34) {
+                    printf("%c", (char)((int)fbuff[i + 2] + 0x47));
+                } else if (fbuff[i + 2] < (char)0x3E) {
+                    printf("%c", (char)((int)fbuff[i + 2] - 0x04));
+                } else {
+                    switch (fbuff[i + 2]) {
+                        case (char)0x3E:
+                            printf("!");
+                            break;
+                        case (char)0x3F:
+                            printf("?");
+                            break;
+                        case (char)0x40:
+                            printf(" ");
+                            break;
+                        case (char)0x41:
+                            printf("\"");
+                            break;
+                        case (char)0x42:
+                            printf("\'");
+                            break;
+                        case (char)0x43:
+                            printf(",");
+                            break;
+                        case (char)0x44:
+                            printf(".");
+                            break;
+                        case (char)0x45:
+                            printf(":");
+                            break;
+                        case (char)0x46:
+                            printf(";");
+                            break;
+                        case (char)0x47:
+                            printf("--");
+                            break;
+                        case (char)0x48:
+                            printf("/");
+                            break;
+                        case (char)0x49:
+                            printf("(");
+                            break;
+                        case (char)0x4A:
+                            printf(")");
+                            break;
+                        case (char)0x4B:
+                            printf("$");
+                            break;
+                        case (char)0x4C:
+                            printf("<");
+                            break;
+                        case (char)0x4D:
+                            printf(">");
+                            break;
+                        case (char)0x4E:
+                            printf("%%");
+                            break;
+                        case (char)0x4F:
+                            printf("[");
+                            break;
+                        case (char)0x50:
+                            printf("]");
+                            break;
+                        case (char)0x51:
+                            printf("YEN");
+                            break;
+                        case (char)0x52:
+                            printf("@Cruz");
+                            break;
+                        case (char)0x53:
+                            printf("#");
+                            break;
+                        case (char)0x54:
+                            printf("_");
+                            break;
+                        case (char)0x55:
+                            printf("|");
+                            break;
+                        case (char)0x56:
+                            printf("^");
+                            break;
+                        case (char)0x57:
+                            printf("Âº");
+                            break;
+                        case (char)0x58:
+                            printf("-");
+                            break;
+                        case (char)0x59:
+                            printf("+");
+                            break;
+                        case (char)0x5A:
+                            printf("=");
+                            break;
+                            
+                    }
+                }
+                
+                break;
+        }
+	}
+
+	free(fbuff);
+
+	return 0;
+}
